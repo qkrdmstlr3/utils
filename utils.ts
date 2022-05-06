56fba3cfb7c34e9817e9680642d4a8deea6d2c35
shellboy <qkrdmstlr3@naver.com>
2022-05-07 00:41:24 +0900

    [refactor] update inOrOut function
    - some is more suitable than find

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 // 배열에 없는 요소라면 넣어주고 있는 요소라면 빼주는 함수
-export const inOrOut = (a: string[], it: string) => (a.find((i) => i === it) ? a.filter((i) => i !== it) : [it, ...a]);
+export const inOrOut = (a: string[], it: string) => (a.some((i) => i === it) ? a.filter((i) => i !== it) : [it, ...a]);
 
 // 중첩 객체를 flat하게 펴주는 함수(key값이 중복되는 경우는 고려하지 않음)
 export const flatObj = <T>(obj: {}): T =>
